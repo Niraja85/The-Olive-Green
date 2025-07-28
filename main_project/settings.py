6c47cf0a6680edd704c48188e067587a62b74021
Niraja85 <tripathi.niraja@gmail.com>
2025-07-28 04:17:04 +0200

    Removed Debug settings from env.py and set it back to true

--- main_project/settings.py
+++ main_project/settings.py
@@ -26,7 +26,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

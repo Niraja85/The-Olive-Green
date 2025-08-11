eb4ca61815d41e3577f49cc62b74281665d1fe5b
Niraja85 <tripathi.niraja@gmail.com>
2025-08-11 16:45:36 +0200

    Removed admin booking from urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.BookingCreateView.as_view(), name='booking'),
     path("manage/", views.BookingListView.as_view(), name='manage_booking'),
-    path("admin/manage/", views.AdminBookingListView.as_view(),
-         name='admin_booking'),
     path("edit/<int:pk>/", views.BookingUpdateView.as_view(),
          name='edit_booking'),
     path("delete/<int:pk>/", views.BookingDeleteView.as_view(),
